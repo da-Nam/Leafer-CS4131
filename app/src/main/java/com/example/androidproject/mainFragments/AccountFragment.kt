ab4ca48d380c946f56ca730d7c5d3cd4976286c2
Nam <ducnamtran07@gmail.com>
2023-03-17 13:18:28 +0800

    Added main fragments

---
+++ app/src/main/java/com/example/androidproject/mainFragments/AccountFragment.kt
@@ -0,0 +1,29 @@
+package com.example.androidproject.mainFragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.androidproject.R
+import com.example.androidproject.databinding.FragmentAccountBinding
+
+class AccountFragment : Fragment() {
+    private var _binding:FragmentAccountBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentAccountBinding.inflate(inflater, container, false)
+        val view = binding.root
+
+        return view
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+}
---
+++ app/src/main/java/com/example/androidproject/mainFragments/SettngsFragment.kt
@@ -0,0 +1,28 @@
+package com.example.androidproject.mainFragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.androidproject.R
+import com.example.androidproject.databinding.FragmentSettngsBinding
+
+class SettngsFragment : Fragment() {
+    private var _binding : FragmentSettngsBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentSettngsBinding.inflate(inflater, container, false)
+        val view  = binding.root
+        return view
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+}
